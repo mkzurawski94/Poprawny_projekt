c2052ad260c584e2a88cec215e47759937db6541
mkzurawski94@gmail.com <DeusVult1!>
2020-06-10 16:36:59 +0200

    v0.3 nowe auto

--- Human.java
+++ Human.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.time.LocalDateTime;
+
 public class Human {
     String lastName;
     String name;
@@ -7,6 +9,8 @@ public class Human {
     Animal animal;
     Phone phone;
     Car car;
+    private Double salary;
+
 
     public Human(String lastName,
                  String name) {
@@ -22,6 +26,28 @@ public class Human {
                 ", weight=" + weight +
                 ", animal=" + animal +
                 ", phone=" + phone +
+                ", car=" + car +
+                ", salary=" + salary +
                 '}';
     }
+
+
+    public Double getSalary() {
+        LocalDateTime myObj = LocalDateTime.now();
+        System.out.println("Your salary at : " + myObj + " is: " + salary);//pobiera aktualną date i czas
+        return salary;
+    }
+
+    void setSalary(Double salary) throws Exception {//wyjątek ujemnego wynagrodzenia
+        if (salary <= 0.0) {
+            throw new Exception("Are you slayer master???");
+        } else {
+            System.out.println("New data sent to HR database");
+            System.out.println("Pleare come and take your docs from miss Halinka");
+            System.out.println("ZUS and US knows about your salary increase");
+            this.salary = salary; // przypisanie nowego wynagrodzenia
+            System.out.println("Your new salary is: " + salary);
+        }
+    }
+
 }
--- Main.java
+++ Main.java
@@ -2,14 +2,16 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Animal szarik = new Animal("dog");
         szarik.name = "szarik";
         System.out.println(szarik);
         szarik.takeForWalk();
         szarik.feed();
         Human me = new Human("Nowak", "jan");
-        me.car = new Car("126p", "Fiat");
-        System.out.println(me.car);
+        me.car = new Car("126p", "Fiat", "blue");
+        me.setSalary(200.0);
+        me.getSalary();
+
     }
 }
